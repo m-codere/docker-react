f451d6fb17e44d4562dfc949b615fbfa87d1dd28
Mark Codere <mark.codere@wexinc.com>
2023-06-26 15:11:50 -0400

    Add new feature text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Boo YAH!!!
+          NEW Feature!
         </p>
         <a
           className="App-link"
